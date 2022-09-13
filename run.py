691dd8e2f53379e92aed3a3373d168cef6daf5a1
nbroad <nicholas@nmbroad.com>
2022-09-13 03:54:39 +0000

    move more inside for loop
    use savecallback
    limit number of folds to run
    push_to_hub feature

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
+import gc
 from pathlib import Path
 from functools import partial
 from datetime import datetime
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
+import torch
 from transformers import (
     Trainer,
     TrainingArguments,
@@ -31,45 +33,36 @@ def main(cfg: DictConfig):
 
     set_seed(t_args.seed)
 
-    if "wandb" in t_args.report_to:
+    USING_WANDB =  "wandb" in t_args.report_to
+    if USING_WANDB:
+        import wandb
+        print("wandb")
         set_wandb_env_vars(cfg)
 
     with t_args.main_process_first():
         dm.prepare_dataset()
 
-    model_config = AutoConfig.from_pretrained(
-        cfg.model.model_name_or_path,
-        problem_type=cfg.data.problem_type,
-        num_labels=len(dm.label2id),
-        label2id=dm.label2id,
-        id2label=dm.id2label,
-        hidden_dropout_prob=cfg.model.hidden_dropout_prob,
-        attention_probs_dropout_prob=cfg.model.attention_probs_dropout_prob,
-    )
-    model_config.update({
-        "multisample_dropout": OmegaConf.to_container(cfg.model.multisample_dropout),
-        "output_layer_norm": cfg.model.output_layer_norm,
-        "classifier_dropout_prob": cfg.model.classifier_dropout_prob,
-        "loss_fn": cfg.model.loss_fn,
-    })
-    
-    ### Set up callbacks ###
-    
-    callbacks = []
-    if "wandb" in t_args.report_to:
-        callbacks.append(NewWandbCB(cfg))
-    
-    callbacks.append(SaveCallback(
-        min_score_to_save=cfg.min_score_to_save, 
-        metric_name=t_args.metric_for_best_model, 
-        weights_only=True,
-    ))
-
     run_start = datetime.utcnow().strftime("%Y-%d-%m_%H-%M-%S")
 
-    for fold in range(cfg.data.kfolds):
+    for fold in range(cfg.folds_to_run):
 
         t_args.output_dir = f"{Path.cwd()/(run_start+'_f'+str(fold))}"
+        
+        model_config = AutoConfig.from_pretrained(
+            cfg.model.model_name_or_path,
+            problem_type=cfg.data.problem_type,
+            num_labels=len(dm.label2id),
+            label2id=dm.label2id,
+            id2label=dm.id2label,
+            hidden_dropout_prob=cfg.model.hidden_dropout_prob,
+            attention_probs_dropout_prob=cfg.model.attention_probs_dropout_prob,
+        )
+        model_config.update({
+            "multisample_dropout": OmegaConf.to_container(cfg.model.multisample_dropout),
+            "output_layer_norm": cfg.model.output_layer_norm,
+            "classifier_dropout_prob": cfg.model.classifier_dropout_prob,
+            "loss_fn": cfg.model.loss_fn,
+        })
 
         train_ds = dm.get_train_dataset(fold)
         eval_ds = dm.get_eval_dataset(fold)
@@ -82,10 +75,26 @@ def main(cfg: DictConfig):
                 dm.tokenizer, pad_to_multiple_of=cfg.data.pad_multiple
             )
         
+        ### Set up callbacks ###
+    
+        callbacks = []
+        if USING_WANDB:
+            callbacks.append(NewWandbCB(cfg))
+
+        callbacks.append(SaveCallback(
+            threshold_score=cfg.threshold_score, 
+            metric_name=t_args.metric_for_best_model,
+            greater_is_better=t_args.greater_is_better,
+            weights_only=True,
+        ))
+        
         if cfg.data.mask_augmentation:
             trainer_class = partial(MaskAugmentationTrainer, cfg=cfg)
         else:
             trainer_class = Trainer
+            
+        metrics = partial(compute_metrics, data_module=dm, data_cfg=cfg.data)
+        
 
         trainer = trainer_class(
             model=model,
@@ -94,13 +103,44 @@ def main(cfg: DictConfig):
             eval_dataset=eval_ds,
             tokenizer=dm.tokenizer,
             data_collator=collator,
-            compute_metrics=partial(compute_metrics, label2id=dm.label2id),
+            compute_metrics=metrics,
             callbacks=callbacks,
         )
         
         trainer.remove_callback(WandbCallback)
+        
 
         trainer.train()
+
+        best_metric_score = getattr(
+            trainer.model.config,
+            f"best_{t_args.metric_for_best_model}", 
+            0
+        )
+        trainer.log({f"best_{t_args.metric_for_best_model}": best_metric_score})
+
+        run_id = wandb.run.id if USING_WANDB else ""
+        run_name = wandb.run.name if USING_WANDB else ""
+        
+        
+        model.config.update({"wandb_id": run_id, "wandb_name": run_name})
+        model.config.save_pretrained(t_args.output_dir)
+
+        if t_args.push_to_hub:
+            push_to_hub(
+                trainer,
+                config=cfg,
+                metrics={f"best_{t_args.metric_for_best_model}": best_metric_score},
+                wandb_run_id=run_id,
+            )
+
+        if USING_WANDB:
+            wandb.finish()
+
+        del trainer, model
+        gc.collect()
+        torch.cuda.empty_cache()
+        
 
 
 if __name__ == "__main__":
